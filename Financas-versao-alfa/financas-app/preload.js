995f3a0d30099ddbd98c3c2d5e2a7d53815c83c0
Souza1932 <renatooliveirasouza830@gmail.com>
2026-08-09 20:30:42 -0300

    Remove Financas-versao-alfa

--- Financas-versao-alfa/financas-app/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('janela', {
-  minimizar: () => ipcRenderer.send('janela-minimizar'),
-  maximizar: () => ipcRenderer.send('janela-maximizar'),
-  fechar:    () => ipcRenderer.send('janela-fechar')
-})
--- Financas-versao-alfa/financas-email-api/src/main/java/com/renato/financas/email/EmailJaCadastradoException.java
+++
@@ -1,8 +0,0 @@
-package com.renato.financas.email;
-
-/** Lançada ao tentar cadastrar um e-mail que já existe. */
-public class EmailJaCadastradoException extends RuntimeException {
-    public EmailJaCadastradoException(String email) {
-        super("Este e-mail já está cadastrado: " + email);
-    }
-}
